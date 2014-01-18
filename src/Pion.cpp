f86347f4121ce39f732bd80d828d5e9658de5477
Vladislav Fitc <vladislavfitz@me.com>
2014-01-18 23:29:41 +0100

    Pion

--- src/Pion.cpp
+++ src/Pion.cpp
@@ -3,3 +3,19 @@
 Pion::Pion(string nom){
     this->nom = nom;
 }
+
+void Pion::setJouer(int j){
+    this->jouer = j;
+}
+
+int Pion::getJouer(){
+    return this->jouer;
+}
+
+void Pion::setSymbole(char s){
+    this->symbole = s;
+}
+
+char Pion::getSymbole(){
+    return this->symbole;
+}
