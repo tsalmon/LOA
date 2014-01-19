5d511165130746e5bd32ac6c3ee4ed4f825c00df
DarkBoby <thedarkboby@gmail.com>
2014-01-19 19:47:06 +0100

    dsqf

--- src/Jeu.hpp
+++ src/Jeu.hpp
@@ -8,18 +8,12 @@ protected:
   int nbColonnes;
   int nbJoueur;
 public:
-  //Jeu(){}
   virtual int fini () =0;
   virtual bool peutJouer () =0;
   virtual void newPartie () =0;
   virtual int getNbLignes()=0;
   virtual int getNbJoueur()=0;
   virtual int getNbColonnes()=0;
-<<<<<<< HEAD
-  //virtual int **deplacementPossible (int x, int Y, string Nom) =0;
   virtual string* getListePions () =0;
-=======
-  virtual int **deplacementPossible (int x, int Y, string Nom) =0;
->>>>>>> dames
 };
 #endif
