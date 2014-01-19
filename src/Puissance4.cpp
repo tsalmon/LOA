5e6a83c3acb47478549d4d962b2a544401eb0604
Vladislav Fitc <vladislavfitz@me.com>
2014-01-19 01:58:15 +0100

    Pour ça qu'il ne râle pas

--- src/Puissance4.cpp
+++ src/Puissance4.cpp
@@ -13,9 +13,11 @@ void Puissance4::newPartie (){
 }
 
 int **Puissance4::deplacementPossible (int x, int Y, string Nom){
-  
+    int **i;
+    return i;
 }
 
 string* Puissance4::getListePions (){
-  
+    string *str;
+    return str;
 }
