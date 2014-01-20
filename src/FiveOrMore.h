d1a4d28c198ce4824aeea8b991cc794e896e57b2
Vladislav Fitc <vladislavfitz@me.com>
2014-01-20 19:38:47 +0100

    FiveOrMore
    Class ajouté

--- src/FiveOrMore.h
+++ src/FiveOrMore.h
@@ -10,9 +10,30 @@
 #define __Projet__FiveOrMore__
 
 #include <iostream>
-#include "Jeu.hpp"
+
+#include "Plateau.hpp"
+#include "Partie.hpp"
+
 class FiveOrMore:public Jeu{
+private:
+    unsigned int nbPions;
+    unsigned int points;
+    virtual int getNbLignes();
+    virtual int getNbJoueur();
+    virtual int getNbColonnes();
+    virtual bool peutJouer ();
+    virtual int fini ();
     
+    vector<Pion*> vp;
+    
+    int **getHorizontalMask(/*Plateau *p,*/ int nbLignes, int nbColonnes);
+    int **getVerticalMask(/*Plateau *p,*/ int nbLignes, int nbColonnes);
+    int **getDiagonalMask1(/*Plateau *p,*/ int nbLignes, int nbColonnes);
+    int **getDiagonalMask2(/*Plateau *p,*/ int nbLignes, int nbColonnes);
+    void checkFive(/*Plateau *p*/);
+public:
+    FiveOrMore(unsigned int dimension, unsigned int nbPions);
+    virtual void newPartie ();
 };
 
 #endif /* defined(__Projet__FiveOrMore__) */
