25408132b3f9ba086a6299073cd5628329238054
Vladislav Fitc <vladislavfitz@me.com>
2014-01-20 17:50:26 +0100

    FiveOrMore
    Sans implémentation encore…

---
+++ src/FiveOrMore.cpp
@@ -0,0 +1,9 @@
+//
+//  FiveOrMore.cpp
+//  Projet
+//
+//  Created by Владислав Фиц on 20.01.14.
+//  Copyright (c) 2014 Vladislav Fitc. All rights reserved.
+//
+
+#include "FiveOrMore.h"
---
+++ src/FiveOrMore.h
@@ -0,0 +1,14 @@
+//
+//  FiveOrMore.h
+//  Projet
+//
+//  Created by Владислав Фиц on 20.01.14.
+//  Copyright (c) 2014 Vladislav Fitc. All rights reserved.
+//
+
+#ifndef __Projet__FiveOrMore__
+#define __Projet__FiveOrMore__
+
+#include <iostream>
+
+#endif /* defined(__Projet__FiveOrMore__) */
